9ad967f1848eef0145b3d1e86baf634aad5a81a3
DanielSanMiguel <146175551+DanielSanMiguel@users.noreply.github.com>
2023-10-23 11:13:05 +0200

    Add files via upload

--- cuestionario.py
+++ cuestionario.py
@@ -8,29 +8,54 @@ Created on Sat Oct 21 21:12:20 2023
 import streamlit as st
 import requests
 import json
-
+#import os
+#from dotenv import load_dotenv
+#load_dotenv('C:\\Users\\Daniel\\Desktop\\Python\\.env')
+#api_key = os.environ.get('Token_flyfut')
 api_key = st.secrets['at_token']
 base_id = 'appFezarrh9fv6WrS'
 table_name = 'cuestionario'
 headers = {"Authorization" : f"Bearer {api_key}",  "Content-Type" : 'application/json' }
-contenedor = st.container()
-name = contenedor.text_input('Nombre')
-incidencias = contenedor.text_input('Incidencias')
-calidad = contenedor.text_input('Calidad del Servicio')
 
+clients = ['analista_1', 'analista_2']
+pilots = ['piloto_1', 'piloto_2']
 
-calificacion = contenedor.select_slider('Calificacion: de 1 (muy mal) a 5 (excelente)',options=[1,2,3,4,5])
-
-b_1 = st.button('ENVIAR')
-if b_1:
-    data = {
-  "records": [
-    {
-      "fields": {
-        "Name": name,
-        "Calidad Servicio": calidad,
-        "Incidencias": incidencias,
-        "Calificacion": calificacion
-      }
-    }]}
-    requests.post(r'https://api.airtable.com/v0/appFezarrh9fv6WrS/cuestionario', json.dumps(data),headers=headers)
+user = st.text_input('Introduzca su nombre completo:')
+if user in clients:
+    contenedor = st.container()
+    incidencias = contenedor.text_input('Incidencias')
+    calidad = contenedor.text_input('Calidad del Servicio')
+    calificacion = contenedor.select_slider('Calificacion: de 1 (muy mal) a 5 (excelente)',options=[1,2,3,4,5])
+    
+    b_1 = st.button('ENVIAR')
+    if b_1:
+        data = {
+      "records": [
+        {
+          "fields": {
+            "Name": user,
+            "Calidad Servicio": calidad,
+            "Incidencias": incidencias,
+            "Calificacion": calificacion
+          }
+        }]}
+        requests.post(r'https://api.airtable.com/v0/appFezarrh9fv6WrS/cuestionario', json.dumps(data),headers=headers)
+elif user in pilots:
+    contenedor_2 = st.container()
+    incidencias = contenedor_2.text_input('Incidencias')
+    calificacion = contenedor_2.select_slider('Calificacion: de 1 (muy mal) a 5 (excelente)',options=[1,2,3,4,5])
+    
+    b_1 = st.button('ENVIAR')
+    if b_1:
+        data = {
+      "records": [
+        {
+          "fields": {
+            "Name": user,
+            "Incidencias": incidencias,
+            "Calificacion": calificacion
+          }
+        }]}
+        requests.post(r'https://api.airtable.com/v0/appFezarrh9fv6WrS/cuestionario', json.dumps(data),headers=headers)
+else:
+    st.write('Nombre incorrecto, vuleva a intentarlo.')
